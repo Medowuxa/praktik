e2d2e175d0515e4e736e7f92d3923673ae38e95a
Натычко Вероника Сергеевна <1015-22@std.pkgh.edu>
2024-12-16 17:08:54 +0300

    tmp

--- src/components/Sub.jsx
+++ src/components/Sub.jsx
@@ -1,43 +1,41 @@
-import React from 'react';
-
-
-const componentName = () => {
-    
-
-  let requestOptions = {
-    method: 'POST',
-    redirect: 'follow'
-  };
-
-  fetch("https://pets.xn--80ahdri7a.site/api/subscription", requestOptions)
-  .then(response=> response.json())  
-        
-   return (
+import { useRef, useState } from "react";
+ 
+const NewsComponent = () => {
+  let [mail, setMail] = useState();
+  let message = useRef()
+  const load = (e) => {
+      e.preventDefault();
+      let requestOptions = {
+          method: 'POST',
+          body:new FormData(document.getElementById("sab")),
+      };
+ 
+      fetch("https://pets.сделай.site/api/subscription", requestOptions)
+      .then(response => response.status)
+      .then(result => {
+          console.log(result);
+          if (result === 204) document.getElementById("message").classList.remove('d-none')
+          else document.getElementById("message").classList.add('d-none')
+      })
+      .catch(error => console.log('error', error));
+    }
+    return (
         <div>
-            <h2 className="text-center text-black bg-success bg-opacity-25 m-2">
-    Подписка на новости
-  </h2>
-  <form className="w-50 m-auto p-3" style={{ minWidth: 300 }}>
-    <div className="mb-3">
-      <label htmlFor="exampleInputEmail1" className="form-label">
-        Введите адрес электронной почты
-      </label>
-      <input
-        type="email"
-        className="form-control"
-        id="exampleInputEmail1"
-        aria-describedby="emailHelp"
-      />
-      <div id="emailHelp" className="form-text">
-        Мы никогда не делимся Вашими e-mail ни с кем.
-      </div>
-    </div>
-    <button type="submit" className="btn btn-success">
-      Подписаться
-    </button>
-  </form>
+          <h2 className="text-center text-white bg-success m-2">Личный кабинет</h2>
+            <form id="sab" className="w-50 m-auto p-3" style={{ 'minWidth': '300px' }}onSubmit={load}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Введите адрес электронной почты</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" onChange={(e)=>setMail({...mail,email:e.target.value})} />
+                    <div id="emailHelp" className="form-text">Мы не делимся Вашими e-mail.</div>
+                </div>
+                <button type="submit" className="btn bg-success text-white">Подписаться</button>
+            </form>
+            <div className="alert  d-none w-50 m-auto p-3 bg-success text-white" role="alert" rev={message} id="message">
+                Вы успешно подписались на новости!
+            </div>
+            <pre/>
         </div>
     );
 };
-
-export default componentName;
+ 
+export default NewsComponent;
